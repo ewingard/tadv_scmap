99854518d1c38ee61a8ce4dc4921241ec0157fb2
Ezra W. <78620375+ewingard@users.noreply.github.com>
2024-11-27 18:09:36 -0500

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import SouthCarolinaMap from '../components/south-carolina-map'
+
+export default function Home() {
+  return (
+    <main className="container mx-auto py-10">
+      <SouthCarolinaMap />
+    </main>
+  )
+}
